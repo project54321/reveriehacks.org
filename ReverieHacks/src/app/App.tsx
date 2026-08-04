1e617bcc9844e93ba8b504d30c12a2e96b42dfbd
Arjun Averineni <arjun.averineni123@gmail.com>
2026-08-04 18:19:19 -0500

    Add Comments.

--- ReverieHacks/src/app/App.tsx
+++ ReverieHacks/src/app/App.tsx
@@ -1,6 +1,8 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
 
+//Entry point for the app. This is where the router is initialized and rendered.
+
 export default function App() {
   return <RouterProvider router={router} />;
 }
